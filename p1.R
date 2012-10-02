9d4945161ea90c12f501d948df5a8ddbc712ab34
Aaron Okano <adokano@ucdavis.edu>
2012-10-02 11:49:37 -0700

    Added stuff

--- p1.R
+++ p1.R
@@ -1,4 +1,4 @@
-callSplash1 <- function( ) {
+coinSplash1 <- function( ) {
   # Create list of values for heads & tails for each coin
   coins <- list( c( 5,-5 ), c( 10,-10 ), c( 25,-25 ) )
   # Simulate 1000 tosses of each coin and find the winnings
@@ -6,4 +6,16 @@ callSplash1 <- function( ) {
   return( length( result[ result >= 20 ] ) / 1000 )
 }
 
-callSplash1()
+coinSplash2 <- function( ) {
+  # Create list of values for heads & tails for each coin
+  coins <- list( c( 5,-5 ), c( 10,-10 ), c( 25,-25 ) )
+  # Simulate 1000 tosses of each coin and find the winnings
+  samples <- lapply( coins, sample, 1000, TRUE )
+  result <- Reduce( "+", samples )
+  return( length(
+                 result[ result == 20 | result == 40 ] ) / length( 
+                 result[ result >= 20 ]) )
+}
+
+coinSplash1()
+coinSplash2()
