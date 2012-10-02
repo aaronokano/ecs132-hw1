3abd0a198f5719eaa2e08aa91410b54f2220dc64
Aaron Okano <adokano@ucdavis.edu>
2012-10-02 14:20:42 -0700

    Did problem 2-a simulation. It's in p2.R

--- p1.R
+++ p1.R
@@ -1,21 +1,20 @@
-coinSplash1 <- function( ) {
+coinSplash1 <- function( w, nreps ) {
   # Create list of values for heads & tails for each coin
   coins <- list( c( 5,-5 ), c( 10,-10 ), c( 25,-25 ) )
   # Simulate 1000 tosses of each coin and find the winnings
-  result <- Reduce( "+", lapply( coins, sample, 1000, TRUE ) )
-  return( length( result[ result >= 20 ] ) / 1000 )
+  result <- Reduce( "+", lapply( coins, sample, nreps, TRUE ) )
+  return( length( result[ result >= w ] ) / nreps )
 }
 
-coinSplash2 <- function( ) {
+coinSplash2 <- function( nreps ) {
   # Create list of values for heads & tails for each coin
   coins <- list( c( 5,-5 ), c( 10,-10 ), c( 25,-25 ) )
   # Simulate 1000 tosses of each coin and find the winnings
-  samples <- lapply( coins, sample, 1000, TRUE )
-  result <- Reduce( "+", samples )
+  result <- Reduce( "+", lapply( coins, sample, nreps, TRUE ) )
   return( length(
                  result[ result == 20 | result == 40 ] ) / length( 
                  result[ result >= 20 ]) )
 }
 
-coinSplash1()
-coinSplash2()
+coinSplash1( 20, 10000)
+coinSplash2( 10000 )
---
+++ p2.R
@@ -0,0 +1,12 @@
+busSuccess <- function( nreps ) {
+  # Create vectors of delays and their respective probabilities
+  delays <- c( 0, 1, 2, 3 )
+  probabilities <- c( 0.5, 0.25, 0.15, 0.1 )
+  # Simulate arrival times and find the difference
+  result <- Reduce( "-", list( sample( delays, nreps, TRUE, probabilities ),
+                              sample( delays, nreps, TRUE, probabilities ) ) )
+  # Return the probability of a successful transfer (i.e. A - B <= 1.25)
+  return( length( result[ result <= 1.25 ] ) / nreps )
+}
+
+busSuccess( 10000 )
