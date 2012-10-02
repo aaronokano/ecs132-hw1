7e5ce7366e063d969fa7a3dad307379fd6209e54
Aaron Okano <adokano@ucdavis.edu>
2012-10-01 20:23:07 -0700

    Commented my code

--- p1.R
+++ p1.R
@@ -1,7 +1,9 @@
 callSplash1 <- function( ) {
+  # Create list of values for heads & tails for each coin
   coins <- list( c( 5,-5 ), c( 10,-10 ), c( 25,-25 ) )
+  # Simulate 1000 tosses of each coin and find the winnings
   result <- Reduce( "+", lapply( coins, sample, 1000, TRUE ) )
-  return( length( result[ result >= 20 ] ) )
+  return( length( result[ result >= 20 ] ) / 1000 )
 }
 
 callSplash1()
