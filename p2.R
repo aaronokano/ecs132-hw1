5db384c75806b3e7d886005536d4cad61a6e9d3b
Anatoly Torchinsky <link13@gmail.com>
2012-10-04 10:52:58 -0700

    Problem 2B

--- p2.R
+++ p2.R
@@ -10,3 +10,15 @@ busSuccess <- function( nreps ) {
 }
 
 busSuccess( 10000 )
+
+busSuccess2 <- function( nreps ) {
+  # Create vectors of delays and their respective probabilities
+  delays <- c( 0, 1, 2, 3 )
+  probabilities <- c( 0.5, 0.25, 0.15, 0.1 )
+  # Simulate arrival times and find the difference
+  result <- Reduce( "-", list( sample( delays, nreps, TRUE, probabilities ),
+                              sample( delays, nreps, TRUE, probabilities ) ) )
+  # Return the probability of a successful transfer (i.e. A - B <= 1.25)
+  return( length( result[ result >= 0.0 ] ) / nreps )
+}
+busSuccess2( 10000 )
