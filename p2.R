09551293549961c2af5777e75ebfdedb3eaa09f0
Aaron Okano <adokano@ucdavis.edu>
2012-10-04 14:28:21 -0700

    Finished

--- p2.R
+++ p2.R
@@ -16,9 +16,12 @@ busSuccess2 <- function( nreps ) {
   delays <- c( 0, 1, 2, 3 )
   probabilities <- c( 0.5, 0.25, 0.15, 0.1 )
   # Simulate arrival times and find the difference
-  result <- Reduce( "-", list( sample( delays, nreps, TRUE, probabilities ),
+  successes <- Reduce( "-", list( sample( delays, nreps, TRUE, probabilities ),
                               sample( delays, nreps, TRUE, probabilities ) ) )
-  # Return the probability of a successful transfer (i.e. A - B <= 1.25)
-  return( length( result[ result >= 0.0 ] ) / nreps )
+  # Create new vector with only successful transfers
+  result <- successes[ successes <= 1.25 ]
+  return( length( result[ result < 0.0 ] ) / length( result ) )
 }
+
+busSuccess( 10000 )
 busSuccess2( 10000 )
